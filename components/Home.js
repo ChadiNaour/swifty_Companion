4af5686a31c251f7fa352fa3f4caa9e407605560
Chadi Naour <naourchadi@gmail.com>
2022-02-20 11:30:50 +0100

    styling home page , still same issues

--- components/Home.js
+++ components/Home.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { View, Text, StyleSheet, Pressable, Image, TextInput, useWindowDimensions, SafeAreaView, Switch, ActivityIndicator } from "react-native";
+import { View, Text, StyleSheet, Pressable, Image, TextInput, useWindowDimensions, SafeAreaView, Switch, ActivityIndicator, ImageBackground } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
 import Home_image from '../assets/42_Logo.svg.png';
 import { user_id, user_secret } from "../config.json";
+import Image_background from "../assets/42.jpeg"
 
 const Home = ({ navigation }) => {
     const [login, onChangeLogin] = React.useState("");
@@ -33,33 +34,32 @@ const Home = ({ navigation }) => {
 
     const sendRequ = async (login, token) => {
         try {
-          var response = await axios.get(
-            "https://api.intra.42.fr/v2/users/" + login.toLowerCase(),
-            {
-              headers: {
-                Authorization: "Bearer " + token.access_token,
-              },
+            var response = await axios.get(
+                "https://api.intra.42.fr/v2/users/" + login.toLowerCase(),
+                {
+                    headers: {
+                        Authorization: "Bearer " + token.access_token,
+                    },
+                }
+            );
+            if (response.data) {
+                var coalition = await axios.get(
+                    "https://api.intra.42.fr/v2/users/" + response.data.id + "/coalitions",
+                    {
+                        headers: {
+                            Authorization: "Bearer " + token.access_token,
+                        },
+                    }
+                )
+                console.log("coalition is", coalition);
+                if (coalition.data)
+                    navigation.navigate("Details", { data: response.data, coalition: coalition.data });
             }
-          );
-          if (response.data)
-          {
-            var coalition = await axios.get(
-              "https://api.intra.42.fr/v2/users/" + response.data.id + "/coalitions",
-              {
-                headers: {
-                  Authorization: "Bearer " + token.access_token,
-                },
-              }
-            )
-            console.log("coalition is",coalition);
-            if (coalition.data)
-                navigation.navigate("Details", { data: response.data, coalition : coalition.data});
-          }
         } catch (error) {
             console.log(error.message);
-          alert("login doesnt exist");
+            alert("login doesnt exist");
         }
-      };
+    };
 
     const fetchLogin = async (login) => {
         setLoading(true);
@@ -68,12 +68,10 @@ const Home = ({ navigation }) => {
                 // var token = await getToken();
                 // await AsyncStorage.setItem("access_token", JSON.stringify(token));
                 var token = await AsyncStorage.getItem("access_token");
-                if (token)
-                {
+                if (token) {
 
                     token = JSON.parse(token);
-                    if((token.created_at + token.expires_in) <= (Date.now() / 1000))
-                    {
+                    if ((token.created_at + token.expires_in) <= (Date.now() / 1000)) {
                         console.log("token expired");
                         token = await getToken();
                         if (token)
@@ -108,38 +106,45 @@ const Home = ({ navigation }) => {
 
     return (
         <SafeAreaView style={styles.container}>
-            <View style={styles.Switch}>
-                <Switch
-                    trackColor={{ false: "#767577", true: "#81b0ff" }}
-                    thumbColor={enabled ? "#f5dd4b" : "#f4f3f4"}
-                    ios_backgroundColor="#3e3e3e"
-                    value={enabled}
-                    onValueChange={toggleSwitch}
-                // onValueChange={() => setTheme(!theme)}
-                // value={!theme}
-                />
-            </View>
+            <ImageBackground
+                source={Image_background}
+                resizeMode="cover"
+                style={{height: height,         paddingTop: 40,}}
+                // style={styles.image}
+            >
+                <View style={styles.Switch}>
+                    <Switch
+                        trackColor={{ false: "#767577", true: "#81b0ff" }}
+                        thumbColor={enabled ? "#f5dd4b" : "#f4f3f4"}
+                        ios_backgroundColor="#3e3e3e"
+                        value={enabled}
+                        onValueChange={toggleSwitch}
+                    // onValueChange={() => setTheme(!theme)}
+                    // value={!theme}
+                    />
+                </View>
 
-            {/* {!loading ? */}
-            <View style={styles.viewContainer}>
-                {/* <Text>sfsfsfsf</Text> */}
-                <Image style={styles.tinyLogo} source={Home_image} resizeMode="cover" />
-                <TextInput
-                    style={styles.input}
-                    onChangeText={onChangeLogin}
-                    value={login}
-                    placeholder="Login"
-                // keyboardType="numeric"
-                />
-                <Pressable
-                    style={styles.button}
-                    // title="Search"
-                    onPress={() => fetchLogin(login)}
-                >
-                    <Text style={styles.text}>Search</Text>
-                </Pressable>
-            </View>
-            {/* : <ActivityIndicator size="large" color="#00ff00" />} */}
+                {/* {!loading ? */}
+                <View style={styles.viewContainer}>
+                    {/* <Text>sfsfsfsf</Text> */}
+                    <Image style={styles.tinyLogo} source={Home_image} resizeMode="cover" />
+                    <TextInput
+                        style={styles.input}
+                        onChangeText={onChangeLogin}
+                        value={login}
+                        placeholder="Login"
+                    // keyboardType="numeric"
+                    />
+                    <Pressable
+                        style={styles.button}
+                        // title="Search"
+                        onPress={() => fetchLogin(login)}
+                    >
+                        <Text style={styles.text}>Search</Text>
+                    </Pressable>
+                </View>
+                {/* : <ActivityIndicator size="large" color="#00ff00" />} */}
+            </ImageBackground>
         </SafeAreaView>
     )
 }
@@ -147,7 +152,6 @@ const Home = ({ navigation }) => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        paddingTop: 40,
         backgroundColor: "white"
         // alignItems: 'center',
         // justifyContent: 'center',
@@ -189,7 +193,7 @@ const styles = StyleSheet.create({
         padding: 15,
         alignItems: "flex-end",
         // backgroundColor:"red"
-      },
+    },
 });
 
 export default Home;
